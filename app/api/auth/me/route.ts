0c3c2e0f1092e6dc2950b67a34e2d942c0b4bfd1
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-11-05 02:04:37 +0700

    Implement Login and Logout api

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import { COOKIE_NAME } from '@/constants'
+import { cookies } from 'next/headers'
+import { verify } from 'jsonwebtoken';
+
+export async function GET() {
+  const cookieStore = cookies()
+  const token = cookieStore.get(COOKIE_NAME)
+
+  if (!token) {
+    return NextResponse.json(
+      { message: 'Unauthorized' },
+      { status: 401 },
+    )
+  }
+
+  const { value } = token
+  const secret = process.env.JWT_SECRET || ''
+
+  try {
+    verify(value, secret)
+    const respone = {
+      user: 'Secret User'
+    }
+    return new Response(JSON.stringify(respone), {
+      status: 200,
+    })
+  } catch (e) {
+    return NextResponse.json(
+      { message: 'Something went wrong. Please check again!' },
+      { status: 400 },
+    )
+  }
+}
