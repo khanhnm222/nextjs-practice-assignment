869389a602763c6b557740e9fc55cd8aa4ac4a09
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-11-05 02:02:59 +0700

    Integrate PostSection component to display the post list on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
 
+import { PostSection } from '@/components'
 import PageHeader from '@/components/page-header/page'
 import styles from './page.module.css'
 
 export default function Home() {
+  const data = [{
+    id: 1,
+    title: 'Man must explore, and this is exploration at its greatest',
+    subTitle: 'Problems look mighty small from 150 miles up',
+    author: 'Start Bootstrap',
+    createdDate: 'September 24, 2014',
+  }]
+
   return (
-    <main className={styles.main}>
+    <>
       <PageHeader
         title='Clean Blog'
-        subHeader=''
-        backgroundImage='images/home-bg.jpg'  
+        subHeader='A Clean Blog Theme by Start Bootstrap'
+        backgroundImage='images/home-bg.jpg'
       />
-      This is Home Page
-    </main>
+      <main className={styles.main + ' mt-[300px]'}>
+        <div className=''>
+          <div className="col-lg-8 col-lg-offset-2 col-md-offset-1 m-auto">
+            {data && data.map(post => (
+              <PostSection
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                createdDate={post.createdDate}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
