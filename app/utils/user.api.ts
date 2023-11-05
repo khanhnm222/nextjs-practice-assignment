7f0e8237923ff44c3ee8f503df874d641e764deb
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-11-05 11:50:14 +0700

    Check Authenticated on Create post page

---
+++ app/utils/user.api.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosError } from "axios"
+import { UserResponse } from "./models/UserModel"
+
+export async function getUser(): Promise<UserResponse> {
+  try {
+    const { data } = await axios.get('api/auth/me')
+
+    return {
+      user: data,
+      error: null
+    }
+  } catch (error) {
+    const errors = error as AxiosError
+
+    return {
+      user: null,
+      error: errors
+    }
+  }
+}
