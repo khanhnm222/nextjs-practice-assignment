33678de535ad203c8f841250b5ead7dab3af3a7d
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-11-06 02:45:19 +0700

    Update API

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
 
   const { username, password } = body
 
-  if (username !== 'admin' && password !== 'admin') {
+  if (username !== 'admin' || password !== 'admin') {
     return NextResponse.json(
       { message: 'Unauthorized' },
       { status: 401 },
