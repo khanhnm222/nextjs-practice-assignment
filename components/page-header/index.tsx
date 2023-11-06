633d77d2e18c81d215dd9512740399a232a87c51
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-11-07 01:01:54 +0700

    Chnage Header style

--- components/page-header/index.tsx
+++ components/page-header/index.tsx
@@ -30,19 +30,23 @@ const PageHeader = ({
           </div>
         </header>
       ) : (
-        <header className="intro-header absolute top-0 z-0 w-full bg-cover bg-center min-h-[400px] text-white" style={{ backgroundImage: `url(${backgroundImage})` }}>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div className="post-heading">
-                  <h1 className="text-[50px]">{title}</h1>
-                  <h2 className="subheading">{subHeader}</h2>
-                  <span className="meta">Posted by <a href="#">{author}</a> on {createdDate}</span>
+        <>
+          {title && (
+            <header className="intro-header absolute top-0 z-0 w-full bg-cover bg-center min-h-[400px] text-white" style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 m-auto">
+                  <div className="post-heading">
+                    <h1 className="text-[50px]">{title}</h1>
+                    <h2 className="subheading">{subHeader}</h2>
+                    <span className="meta">Posted by <a href="#">{author}</a> on {createdDate}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </header>
+          </header>
+          )}
+        </>
       )}
     </>
   )
