90acf47669ab10f4c49980f25927442435be3da3
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-11-08 01:33:01 +0700

    Update About page header image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,8 +14,8 @@ const About = () => {
         <div className="py-16 bg-white">
           <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
             <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-              <div className="md:5/12 lg:w-5/12">
-                <Image src="/images/blog-about.jpg" alt="image" loading="lazy" width={700} height={420} className="rounded-md" />
+              <div className="md:5/12 lg:w-5/12 flex self-start">
+                <Image src="/images/about-bg-1.jpg" alt="image" loading="lazy" width={700} height={420} className="rounded-md" />
               </div>
               <div className="md:7/12 lg:w-6/12">
                 <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">NextJs 13 development is carried out by passionate developers</h2>
